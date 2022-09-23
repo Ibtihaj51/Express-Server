5abdae8df318b11641bae57f5d9572530196b862
Muhammad Abdullah <equamarlex@gmail.com>
2022-09-23 19:10:07 +0500

    change

--- server.mjs
+++ server.mjs
@@ -4,8 +4,10 @@ const port = 3000
 
 app.get('/', (req, res) => {
     console.log("hello babs");
-  res.send('Hi check this out! = <a href=" https://ibtihaj51.github.io/Charity-Website/ "> https://ibtihaj51.github.io/Charity-Website/')
+  
   console.log('response from ibtihaj ali: ' +  req.ip);
+  console.log("some changes");
+  res.send('Hi check this out! = <a href=" https://ibtihaj51.github.io/Charity-Website/ "> https://ibtihaj51.github.io/Charity-Website/' + req.ip)
 
 })
 
